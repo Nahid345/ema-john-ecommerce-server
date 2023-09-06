d49966c33a900b05a23bef6c177bdffc6e2e9ff4
Nahid345 <nahidparvej07@gmail.com>
2023-09-06 13:32:04 +0600

    loaded data & displaying the products

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const app = express()
+const port = process.env.PORT || 5000;
+
+
+// middleware
+
+app.use(cors())
+app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nkgxtfm.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run(){
+
+    try{
+          
+        const productCollection =client.db('emajohn').collection('products')
+
+        app.get('/products', async(req,res) =>{
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            console.log(page,size);
+            const query = {}
+            const cursor = productCollection.find(query);
+            const products = await cursor.skip(page*size).limit(size).toArray();
+            const count = await productCollection.estimatedDocumentCount(cursor);
+            res.send({count,products});
+        });
+
+        app.post('/productsByIds', async(req,res) =>{
+            const ids = req.body;
+            console.log(ids);
+            const obejctIds = ids.map(id => new ObjectId(id))
+            const query = {_id: {$in: obejctIds}};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+
+        })
+
+    }
+
+    finally{
+
+    }
+
+}
+
+run().catch(err => console.log(err))
+
+
+
+
+app.get('/', (req,res) => {
+    res.send('Ema john exommerce server is running');
+});
+
+app.listen(port, () =>{
+    console.log(`ema john server is running on: ${port}`)
+})
